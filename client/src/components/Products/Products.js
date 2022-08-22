d2fa754af23c08a3c918e6e422a6b94d20a6bf3f
Andy Gorbould <gorbould@mail.com>
2022-08-22 16:06:15 +0200

    added index for product

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 // import styles from "./Products.module.css";
 
 function Products() {
+  const i = 1;
   const [data, setData] = useState(null);
   useEffect(() => {
     fetch("http://localhost:3001/products")
@@ -21,15 +22,15 @@ function Products() {
   return (
     <div>
       <p>Products</p>
-      {!data ? "Loading Title...." : <h4>{data[7].Title}</h4>}
+      {!data ? "Loading Title...." : <h4>{data[i].Title}</h4>}
       {!data ? (
         "Loading Image...."
       ) : (
-        <img src={imagePath(data[7].IMG_SRC)} alt={data[7].IMG_ALT} />
+        <img src={imagePath(data[i].IMG_SRC)} alt={data[i].IMG_ALT} />
       )}
-      {!data ? "Loading Description...." : <p>{data[7].Description}</p>}
-      {!data ? "Loading Price...." : <p>{data[7].Price}</p>}
-      {!data ? "Loading Category...." : <p>{data[7].Category}</p>}
+      {!data ? "Loading Description...." : <p>{data[i].Description}</p>}
+      {!data ? "Loading Price...." : <p>{data[i].Price}</p>}
+      {!data ? "Loading Category...." : <p>{data[i].Category}</p>}
     </div>
   );
 }
