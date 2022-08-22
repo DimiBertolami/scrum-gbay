f646c796af245e99137d86826a6e74ffce5a9ad8
Andy Gorbould <gorbould@mail.com>
2022-08-22 14:54:54 +0200

    fixed merge

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,11 @@ import "./App.css";
 import React from "react";
 import Products from "./components/Products/Products";
 
-// function App() {
-//   const [data, setData] = React.useState(null);
-//   React.useEffect(() => {
-//     fetch("http://localhost:3001/users")
-//       .then((res) => res.json())
-//       .then((data) => {
-//         console.log(data);
-//         setData(data);
-//       });
-//   }, []);
-//   return (
-//     <div className="app">
-//       <p>{!data ? "Loading..." : data[0].FirstName}</p>
-//       <h1>Hello world!</h1>
-//     </div>
-//   );
-// }
+// import FormInput from "./components/FormInput";
+import Header from "./components/Header";
+import Products from './components/Products/Products';
+
+
 
 function App() {
   return (
--- server/models/Database.js
+++ server/models/Database.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 require("dotenv").config();
-
 console.log(process.env.DB_DATABASE);
 class Database {
   constructor() {
     this.con = mysql.createConnection({
+      port: process.env.DB_PORT,
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
