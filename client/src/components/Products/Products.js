8012b1d94863e082e79afd4a2e03c53550aa616a
Andy Gorbould <gorbould@mail.com>
2022-08-23 13:40:08 +0200

    products js cleaned unused code and comments

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -23,7 +23,7 @@ function Products() {
     return (
       <div>
         <h1>Loading....</h1>{" "}
-        {/* 👈 replace this with a spinny-loader component?? example: https://mui.com/material-ui/react-progress/ */}
+        {/* 👆 replace this with a spinny-loader component?? example: https://mui.com/material-ui/react-progress/ */}
       </div>
     );
   }
@@ -38,48 +38,6 @@ function Products() {
       <p>{data[i].Category}</p>
     </div>
   );
-
-  // // old return statment needs to be an 'if'
-  // return (
-  //   <div>
-  //     <p>Products</p>
-  //     {!data ? "Loading Title...." : <h4>{data[i].Title}</h4>}
-  //     {!data ? (
-  //       "Loading Image...."
-  //     ) : (
-  //       // 👇 calls the 'imagePath' function with the IMG_SRC as a prop 👇
-  //       <img src={imagePath(data[i].IMG_SRC)} alt={data[i].IMG_ALT} />
-  //     )}
-  //     {!data ? "Loading Description...." : <p>{data[i].Description}</p>}
-  //     {!data ? "Loading Price...." : <p>{data[i].Price}</p>}
-  //     {!data ? "Loading Category...." : <p>{data[i].Category}</p>}
-  //   </div>
-  // );
 }
-// // This is just to make sure it's working when needed
-// //
-// function Products() {
-//   return (
-//     <div>tessst - client\src\components\Products\Products.js</div>
-//   )
-// }
-// //
-// // End of test
 
-// // //
-//
-// return (
-//   <div>
-//     <p>Products</p>
-//     {!data ? "Loading Title...." : <h4>{data[i].Title}</h4>}
-//     {!data ? (
-//       "Loading Image...."
-//     ) : (
-//       <img src={imagePath(data[i].IMG_SRC)} alt={data[i].IMG_ALT} />
-//     )}
-//     {!data ? "Loading Description...." : <p>{data[i].Description}</p>}
-//     {!data ? "Loading Price...." : <p>{data[i].Price}</p>}
-//     {!data ? "Loading Category...." : <p>{data[i].Category}</p>}
-//   </div>
-// );
 export default Products;
