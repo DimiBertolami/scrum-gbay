5f7f7aa3e2156908891be4ed76b68739ac144086
Andy Gorbould <gorbould@mail.com>
2022-08-23 13:36:06 +0200

    Products return is betterized

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -19,20 +19,42 @@ function Products() {
     return "/img/" + imgPath;
   }
 
+  if (!data) {
+    return (
+      <div>
+        <h1>Loading....</h1>{" "}
+        {/* 👈 replace this with a spinny-loader component?? example: https://mui.com/material-ui/react-progress/ */}
+      </div>
+    );
+  }
   return (
     <div>
       <p>Products</p>
-      {!data ? "Loading Title...." : <h4>{data[i].Title}</h4>}
-      {!data ? (
-        "Loading Image...."
-      ) : (
-        <img src={imagePath(data[i].IMG_SRC)} alt={data[i].IMG_ALT} />
-      )}
-      {!data ? "Loading Description...." : <p>{data[i].Description}</p>}
-      {!data ? "Loading Price...." : <p>{data[i].Price}</p>}
-      {!data ? "Loading Category...." : <p>{data[i].Category}</p>}
+      <h4>{data[i].Title}</h4>
+      {/* 👇 calls the 'imagePath' function with the IMG_SRC as a prop 👇 */}
+      <img src={imagePath(data[i].IMG_SRC)} alt={data[i].IMG_ALT} />
+      <p>{data[i].Description}</p>
+      <p>{data[i].Price}</p>
+      <p>{data[i].Category}</p>
     </div>
   );
+
+  // // old return statment needs to be an 'if'
+  // return (
+  //   <div>
+  //     <p>Products</p>
+  //     {!data ? "Loading Title...." : <h4>{data[i].Title}</h4>}
+  //     {!data ? (
+  //       "Loading Image...."
+  //     ) : (
+  //       // 👇 calls the 'imagePath' function with the IMG_SRC as a prop 👇
+  //       <img src={imagePath(data[i].IMG_SRC)} alt={data[i].IMG_ALT} />
+  //     )}
+  //     {!data ? "Loading Description...." : <p>{data[i].Description}</p>}
+  //     {!data ? "Loading Price...." : <p>{data[i].Price}</p>}
+  //     {!data ? "Loading Category...." : <p>{data[i].Category}</p>}
+  //   </div>
+  // );
 }
 // // This is just to make sure it's working when needed
 // //
