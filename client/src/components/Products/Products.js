5402952218e79cfd60470e29b6d406ef1521606e
Andy Gorbould <gorbould@mail.com>
2022-08-23 17:14:08 +0200

    all products section working but looking rancid

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { useEffect, useState } from "react";
-// import styles from "./Products.module.css";
+import styles from "./Products.module.css";
 
 function Products() {
-  const i = 1;
+  // const i = 1;
   const [data, setData] = useState(null);
   useEffect(() => {
     fetch("http://localhost:3001/products")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setData(data);
+        console.log(data);
       });
   }, []);
 
@@ -19,23 +19,22 @@ function Products() {
     return "/img/" + imgPath;
   }
 
-  if (!data) {
-    return (
-      <div>
-        <h1>Loading....</h1>{" "}
-        {/* 👆 replace this with a spinny-loader component?? example: https://mui.com/material-ui/react-progress/ */}
-      </div>
-    );
-  }
   return (
     <div>
-      <p>Products</p>
-      <h4>{data[i].Title}</h4>
-      {/* 👇 calls the 'imagePath' function with the IMG_SRC as a prop 👇 */}
-      <img src={imagePath(data[i].IMG_SRC)} alt={data[i].IMG_ALT} />
-      <p>{data[i].Description}</p>
-      <p>{data[i].Price}</p>
-      <p>{data[i].Category}</p>
+      {data ? (
+        data.map((product, key) => (
+          <div className={styles.productContainer} key={key}>
+            <h4>{product.Title}</h4>
+            {/* 👇 calls the 'imagePath' function with the IMG_SRC as a prop 👇 */}
+            <img src={imagePath(product.IMG_SRC)} alt={product.IMG_ALT} />
+            <p>{product.Description}</p>
+            <p>{product.Price}</p>
+            <p>{product.Category}</p>
+          </div>
+        ))
+      ) : (
+        <h1>loading</h1>
+      )}
     </div>
   );
 }
