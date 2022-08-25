901283d91586997ed70d8df254fa84606c7c30b4
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-08-25 12:15:51 +0200

    Add Header component, basic nav bar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import './Headre.css';
+
 
 function Header() {
     return (
-        <div>header</div>
+        <div className="header">
+            <nav>
+                <ul>
+                    <li className="logo"><a href="#">LOGO</a></li>
+                    <li className="links">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="products">Products</a></li>
+                        <li><a href="order">Order Form</a></li>
+                    </li>
+
+                </ul>
+            </nav>
+
+
+        </div>
     );
 }
 
