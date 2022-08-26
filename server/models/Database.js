8334d08bb09c097afd85b29f403c29f0209f4559
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-08-26 16:40:58 +0200

    Database.js

--- server/models/Database.js
+++ server/models/Database.js
@@ -1,77 +1,69 @@
 const mysql = require("mysql");
-<<<<<<< HEAD
+
 const envdata = require("dotenv");
 envdata.config({
   path: ".env",
 });
 console.log(process.env.DB_DATABASE);
-class Database {
-  constructor() {
-    // this.con = mysql.createConnection({
-    //   host: "localhost",
-    //   user: "root",
-    //   password: "",
-    //   database: "gbay",
-    // });
 
+    require("dotenv").config();
 
-=======
-require("dotenv").config();
+    console.log(process.env.DB_DATABASE);
 
-console.log(process.env.DB_DATABASE);
-class Database {
-  constructor() {
->>>>>>> production
-    this.con = mysql.createConnection({
-      port: process.env.DB_PORT,
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-    });
+    class Database {
+      constructor() {
 
-    this.con.connect(function (err) {
-      if (err) throw err;
-      console.log("Connected!");
-    });
-  }
+        this.con = mysql.createConnection({
+          port: process.env.DB_PORT,
+          host: process.env.DB_HOST,
+          user: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_DATABASE,
+        });
 
-  async getAllUsers() {
-    return new Promise((resolve, reject) => {
-      this.con.query("SELECT * from users", (err, result) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(result);
-        }
-      });
-    });
-  }
+        this.con.connect(function (err) {
+          if (err) throw err;
+          console.log("Connected!");
+        });
+      }
 
-  async getUserById(id) {
-    return new Promise((resolve, reject) => {
-      this.con.query("SELECT * from users WHERE id = " + id, (err, result) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(result);
-        }
-      });
-    });
-  }
+      async getAllUsers() {
+        return new Promise((resolve, reject) => {
+          this.con.query("SELECT * from users", (err, result) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(result);
+            }
+          });
+        });
+      }
 
-  // get all products
-  async getAllProducts() {
-    return new Promise((resolve, reject) => {
-      this.con.query("SELECT * from products", (err, result) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(result);
-        }
-      });
-    });
-  }
-}
+      async getUserById(id) {
+        return new Promise((resolve, reject) => {
+          this.con.query("SELECT * from users WHERE id = " + id, (err, result) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(result);
+            }
+          });
+        });
+      }
+
+      // get all products
+      async getAllProducts() {
+        return new Promise((resolve, reject) => {
+          this.con.query("SELECT * from products", (err, result) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(result);
+            }
+          });
+        });
+      }
+    }
+
 const db = new Database();
 exports.Database = db;
